c1abfc0556de93ece2770228f84f6dded3bb1737
Rex <darkorter.fr@gmail.com>
2022-12-20 18:09:23 +0100

    Initial Commit

---
+++ src/main/java/student/customjavacms/App.java
@@ -0,0 +1,13 @@
+package student.customjavacms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
