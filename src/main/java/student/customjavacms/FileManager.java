c6da0857f361ff091865a646035a6b126ff7e533
Rex <darkorter.fr@gmail.com>
2022-12-21 14:24:19 +0100

    menu rework, added selection

---
+++ src/main/java/student/customjavacms/FileManager.java
@@ -0,0 +1,56 @@
+package student.customjavacms;
+import java.io.File; 
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class FileManager {
+
+  public static void CreateFile(String filename) {
+    try {
+      File f = new File(filename);
+      if (f.createNewFile()) {
+        System.out.println("File created: " + f.getName());
+      } else {
+        System.out.println("File already exists.");
+      }
+
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+    }
+  }
+
+
+  public static String SelectFile(String filename){
+    Scanner scf = new Scanner(System.in);
+    File f = new File(filename);
+    if(f.exists() && !f.isDirectory()) { 
+      System.out.println("File has been selected");
+      
+      return filename;
+    } else{
+      System.out.println("File doesn't exist, would you like to create it ? y/n");
+      String ChoiceCreate = scf.nextLine();
+      switch (ChoiceCreate.toLowerCase()) {
+        case "y":
+        case "yes":
+          System.out.println("File will be created...");
+          FileManager.CreateFile(filename);
+          System.out.println("File has been selected");
+          return filename;
+        
+        default:
+          System.out.println("This command does not exists");
+          
+
+        case "n":
+        case "no":
+          System.out.println("File will not be created.");
+          return null;
+      
+      }
+
+    }
+    
+  }
+}
--- src/main/java/student/customjavacms/Main.java
+++ src/main/java/student/customjavacms/Main.java
@@ -2,42 +2,65 @@ package student.customjavacms;
 import java.util.Scanner;
 
 /**
- * Hello world!
- *
+ * CJCMS
+ * Made by AimelRex
  */
 public class Main 
 {
     public static void main( String[] args )
     {
         System.out.println("=======================================================\n ______       __     ______     __    __     ______    \n/\\  ___\\     /\\ \\   /\\  ___\\   /\\ '-./  \\   /\\  ___\\   \n\\ \\ \\____   _\\_\\ \\  \\ \\ \\____  \\ \\ \\-./\\ \\  \\ \\___  \\  \n \\ \\_____\\ /\\_____\\  \\ \\_____\\  \\ \\_\\ \\ \\_\\  \\/\\_____\\ \n  \\/_____/ \\/_____/   \\/_____/   \\/_/  \\/_/   \\/_____/ \n");
-        System.out.println("Welcome to CJCMS !\nA Custom Java Content Manager System\nMade by AimelRex\n=======================================================");
+        System.out.println("Welcome to CJCMS !\nA Custom Java Content Manager System\nMade by AimelRex\nType help to show commands\n=======================================================");
         boolean done = false;
+        String SelectedFile = null;
         Scanner sc = new Scanner(System.in);
         while (!done){
-            System.out.println( "\nWhat would you like to do ? (type help to show commands)" );
+            System.out.println( "\nWhat would you like to do ?" );
             String answer = sc.next();
             switch (answer.toLowerCase()){
+                case "h":
                 case "help":
                     System.out.println("\nWelcome to the help menu, just press enter to exit this menu.");
+                    System.out.println("\nhelp      >   show you all commands and their use");
+                    System.out.println("\ncreate    >   create a new file and ask you the name of it");
+                    System.out.println("\nselect    >   select a file to work on it");
+                    System.out.println("\nmodify    >   enter the modify menu");
+                    System.out.println("\nstop      >   close the program");
                     sc.nextLine();
                     break;
 
                 case "create":
-                    System.out.println("Enter a file name :");
-                    String filename = sc.next();
-                    CreateFile.main(filename);
+                    System.out.println("Enter a file name to create it :");
+                    String CreatedFile = sc.next();
+                    FileManager.CreateFile(CreatedFile);
+                    break;
+                
+                case "select":
+                    System.out.println("Enter a file name to select it :");
+                    SelectedFile = sc.next();
+                    SelectedFile = FileManager.SelectFile(SelectedFile);
+                    break;
+                
+                case "mod":
+                case "modify":
+                    if (SelectedFile == null){
+                        System.out.println("You have to select a file before modifying it.");
+                    } else {
+                        System.out.println("Yaaay");
+                    }
+                    
                     break;
 
+                case "close":
                 case "stop":
                     System.out.println("Goodbye ! Thanks for using CJCMS.");
                     done = true;
                     break;
 
                 default:
-                    System.out.println("Type help to show commands.");
+                    System.out.println("This command does not exists, type help to show commands.");
                     break;
             }
         }
-        sc.close();
     }
 }
